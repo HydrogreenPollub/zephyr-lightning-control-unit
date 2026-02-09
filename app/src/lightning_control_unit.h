78fc79fa69735d66cd2d8efcd1d7cce7e21ec620
dawid288 <daw.pisar@interia.pl>
2026-02-09 21:41:54 +0100

    lcu: add lights struct

--- app/src/lightning_control_unit.h
+++ app/src/lightning_control_unit.h
@@ -9,21 +9,28 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/led_strip.h>
 #include "gpio.h"
 #include "can.h"
+#include "led_strip_ctrl.h"
 #include "can_ids.h"
 
-typedef struct {
+#define STRIP_NUM_PIXELS	DT_PROP(DT_ALIAS(led_strip), chain_length)
+
+typedef struct lcu_can_t{
     const struct device *can_device;
     struct gpio_dt_spec can_tx_led;
     struct gpio_dt_spec can_rx_led;
 }lcu_can_t;
 
-void lcu_lights_driver_thread(void *p1, void *p2, void *p3);
-void lcu_lights_logic_thread(void *p1, void *p2, void *p3);
+typedef struct lcu_lights_t{
+    const struct device *const strip;
+    struct led_rgb pixels[STRIP_NUM_PIXELS];
+    uint8_t lights_mask;
+    uint8_t num_pixels;
+}lcu_lights_t;
 
-void lcu_can_init();
-
+void lcu_init();
 
 
 
--- app/src/main.c
+++ app/src/main.c
@@ -1,69 +1,21 @@
 
 #include <errno.h>
 #include <string.h>
-
-#define LOG_LEVEL 4
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main);
-
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/led_strip.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/spi.h>
 #include <zephyr/sys/util.h>
 #include "lightning_control_unit.h"
 
-#define STRIP_NODE		DT_ALIAS(led_strip)
-
-#define SAMPLE_LED_UPDATE_DELAY 10
-
-#define SAMPLE_LED_BRIGHTNESS 0xFF
-
-#define STRIP_NUM_PIXELS	DT_PROP(DT_ALIAS(led_strip), chain_length)
+LOG_MODULE_REGISTER(main);
 
 
-#define DELAY_TIME K_MSEC(SAMPLE_LED_UPDATE_DELAY)
-
-#define RGB(_r, _g, _b) { .r = (_r), .g = (_g), .b = (_b) }
-
-static const struct led_rgb colors[] = {
-    RGB(SAMPLE_LED_BRIGHTNESS, 0x00, 0x00), /* red */
-    RGB(0x00, SAMPLE_LED_BRIGHTNESS, 0x00), /* green */
-    RGB(0x00, 0x00, SAMPLE_LED_BRIGHTNESS), /* blue */
-};
-
-static struct led_rgb pixels[STRIP_NUM_PIXELS];
-
-static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
 
 int main(void)
 {
-    // size_t color = 0;
-    // int rc;
-    //
-    // if (device_is_ready(strip)) {
-    //     LOG_INF("Found LED strip device %s", strip->name);
-    // } else {
-    //     LOG_ERR("LED strip device %s is not ready", strip->name);
-    //     return 0;
-    // }
-    //
-    // LOG_INF("Displaying pattern on strip");
+    lcu_init();
     while (1) {
-        // for (size_t cursor = 0; cursor < ARRAY_SIZE(pixels); cursor++) {
-        //     memset(&pixels, 0x00, sizeof(pixels));
-        //     memcpy(&pixels[cursor], &colors[color], sizeof(struct led_rgb));
-        //
-        //     rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
-        //     if (rc) {
-        //         LOG_ERR("couldn't update strip: %d", rc);
-        //     }
-        //
-        //     k_sleep(DELAY_TIME);
-        // }
-        //
-        // color = (color + 1) % ARRAY_SIZE(colors);
+        k_sleep(K_FOREVER);
     }
-
-    return 0;
 }
